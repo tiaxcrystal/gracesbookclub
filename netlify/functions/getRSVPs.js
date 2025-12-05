cb9e6b5d54148131ea98ca357f269378ac944afa
tiaxcrystal <tiaxcrystal@gmail.com>
2025-12-05 02:01:49 -0500

    Create getRSVPs.js

---
+++ netlify/functions/getRSVPs.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const dataPath = path.join(__dirname, 'data.json');
+
+exports.handler = async function() {
+  const data = JSON.parse(fs.readFileSync(dataPath));
+  return { statusCode: 200, body: JSON.stringify(data.rsvps) };
+};
