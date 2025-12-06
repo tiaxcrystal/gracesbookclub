af0da37992831581dd602a9fd97c7c733e03aa2d
tiaxcrystal <tiaxcrystal@gmail.com>
2025-12-06 01:19:19 -0500

    Update getBookList.js

--- netlify/functions/getBookList.js
+++ netlify/functions/getBookList.js
@@ -1,9 +1,37 @@
-const fs = require('fs');
-const path = require('path');
-const dataPath = path.join(__dirname, 'data.json');
+const { google } = require('googleapis');
 
-exports.handler = async function() {
-  const data = JSON.parse(fs.readFileSync(dataPath));
-  const books = Object.keys(data.ratings);
-  return { statusCode: 200, body: JSON.stringify(books) };
+exports.handler = async function(event, context) {
+  if (event.httpMethod !== 'GET') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT),
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    });
+
+    const sheets = google.sheets({ version: 'v4', auth });
+    const spreadsheetId = '1-86BOIuWOeINJVJkNojHo-c1T3Ygxm1AgLyedEplgvY';
+
+    // Get the Title column (B) from the Book List tab
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range: 'Book List!B2:B' // skip header row
+    });
+
+    const rows = res.data.values || [];
+    const titles = rows.map(r => r[0]).filter(Boolean); // only non-empty titles
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(titles)
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Could not fetch book list.' })
+    };
+  }
 };
