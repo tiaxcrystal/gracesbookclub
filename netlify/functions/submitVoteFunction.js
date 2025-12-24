a5eebbd4abb92d7d8506defe087503304102a910
tiaxcrystal <tiaxcrystal@gmail.com>
2025-12-24 01:22:16 -0500

    Update submitVoteFunction.js

--- netlify/functions/submitVoteFunction.js
+++ netlify/functions/submitVoteFunction.js
@@ -1,5 +1,5 @@
 const crypto = require('crypto');
-const { getSupabaseAdmin } = require('../supabaseClient');
+const { getSupabaseAdmin } = require('../supabase/supabaseClient');
 
 const MAX_VOTES = 3;
 const COOKIE_NAME = 'graces_vote_session';
